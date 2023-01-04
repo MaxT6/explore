38fa80a75da557ff5778031f7e1ad9b7ee15f0f8
MaxT6 <maximiliant6@gmail.com>
2023-01-04 15:17:15 -0500

    created the Offeres component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero"; 
+import Offers from "./components/Offers";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Offers />
     </div>
   );
 }
---
+++ src/components/Offers.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Offers = () => {
+  return (
+    <div class='max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between'>
+      <p class='text-lg font-bold text-gray-700'>Booking Incentives</p>
+      <p class='text-lg font-bold text-gray-700'>Competitive Rates</p>
+      <p class='text-lg font-bold text-gray-700'>Book with Flexibility</p>
+      <p class='text-lg font-bold text-gray-700'>Trip Insurance</p>
+    </div>
+  )
+}
+
+export default Offers
