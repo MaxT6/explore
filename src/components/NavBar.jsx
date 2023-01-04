49188bfb25ca466ce26da42b52d35e531edc73be
MaxT6 <maximiliant6@gmail.com>
2023-01-04 14:11:48 -0500

    created NavBar component and installed react icons

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HiMenuAlt3 } from 'react-icons/hi'; // Have to put the two beginning letters of icon imprt after the react-icons
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1> Experiences</h1>
+      <HiMenuAlt3 size={25} />
+
+    </div>
+  )
+}
+
+export default NavBar
